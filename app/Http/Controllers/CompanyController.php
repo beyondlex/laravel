6e6948ecea35ef669073231a36cb213bf53c7b77
lex <beyondsnk@163.com>
2017-10-26 22:07:28 +0800

    mail and mailable horizon assets

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Admin;
+use App\Company;
+use App\Mail\CompanyCreated;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    //
+    public function create() {
+        /** @var Admin $admin */
+        $admin = factory(Admin::class)->create();
+        $company = Company::find($admin->company_id);
+        \Mail::to('beyondsnk@163.com')->send(new CompanyCreated($company));
+    }
+}
---
+++ app/Mail/CompanyCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Company;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CompanyCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $company;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Company $company
+     */
+    public function __construct(Company $company)
+    {
+        //
+        $this->company = $company;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.company.created');
+    }
+}
