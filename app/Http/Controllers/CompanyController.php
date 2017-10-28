9cf8b689374f7661c6ea3483edf458bb5bec9ee7
lex <beyondsnk@163.com>
2017-10-28 09:43:34 +0800

    send mail later

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -6,6 +6,8 @@ use App\Admin;
 use App\Company;
 use App\Mail\AdminCreated;
 use App\Mail\CompanyCreated;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -14,8 +16,11 @@ class CompanyController extends Controller
     public function create() {
         /** @var Admin $admin */
         $admin = factory(Admin::class)->create();
+        $user = User::find(1);
 //        $company = Company::find($admin->company_id);
-//        \Mail::to('649981596@qq.com')->queue(new CompanyCreated($company));
-        \Mail::to('649981596@qq.com')->queue(new AdminCreated($admin));
+
+        $when = Carbon::now()->addMinute(1);
+
+        \Mail::to($user)->later($when, new AdminCreated($admin));
     }
 }
