f625b4dd425b644ee45a18b558f7ba9858627847
lex <beyondsnk@163.com>
2017-10-28 18:39:06 +0800

    Notification using queue

--- app/Notifications/OrderPaid.php
+++ app/Notifications/OrderPaid.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class OrderPaid extends Notification
+class OrderPaid extends Notification implements ShouldQueue
 {
     use Queueable;
 
